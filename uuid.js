b91a79a4a44d62c07dfdb4c7813cf389651731a9
Michel TURPIN <michel.turpin1@gmail.com>
2025-03-04 16:52:24 +0100

    feat: UUID V7

--- uuid.js
+++ uuid.js
@@ -1,1 +1,31 @@
-export const randomUUID = crypto.randomUUID?(()=>crypto.randomUUID()):((r,h)=>(n=>(s=>()=>'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g,c=>s[c]()))({x:()=>h[n()],y:()=>h[n()%4+8]}))(()=>r.next().value))((function*(b){while(!0){for(var i of crypto.getRandomValues(b)){yield Math.trunc(i/16);yield i%16;}}})(new Uint8Array(256)),'0123456789abcdef');
+function* randomNumberGenerator(buffer) {
+  while(true) {
+    for(const byte of crypto.getRandomValues(buffer)) {
+      yield Math.trunc(byte/16);
+      yield byte%16;
+    }
+  }
+}
+
+function uuidV7Timestamp() {
+  const unpaddedTimestamp = new Date().getTime().toString(16);
+  const timestamp = ('000000000000' + unpaddedTimestamp).substring(unpaddedTimestamp.length);
+  return `${timestamp.substring(0,8)}-${timestamp.substring(8)}`
+}
+
+const hexAlphabet = '0123456789abcdef';
+const randomNumbers = randomNumberGenerator(new Uint8Array(256));
+const hexDigitGenerators = {
+  x: () => hexAlphabet[randomNumbers.next().value],
+  y: () => hexAlphabet[randomNumbers.next().value % 4 + 8],
+}
+
+function withHexDigit(character) {
+  return hexDigitGenerators[character]();
+}
+
+export const randomUUIDV4 = crypto.randomUUID
+  ? (()=>crypto.randomUUID())
+  : (()=>'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, withHexDigit))
+
+export const randomUUIDV7 = (() => uuidV7Timestamp() + '-7xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, withHexDigit))
